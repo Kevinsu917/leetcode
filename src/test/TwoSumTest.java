c60ebff552622ef6c71f257f8f2b4bae49978306
kevinsu917 <kevinsu917@gmail.com>
2015-02-12 17:59:20 +0800

    new project

---
+++ src/test/TwoSumTest.java
@@ -0,0 +1,30 @@
+package test;
+
+import java.TwoSum;
+
+import org.junit.Test;
+
+/**
+ * @author KevinSu kevinsu917@126.com
+ * @create 2015年2月12日 下午5:40:41
+ * @Description: 
+ */
+public class TwoSumTest {
+
+	@Test
+	public void testtwoSum()
+	{
+		int[] array = {-1,7,11, 8,5,1,4,15};
+		int target = 9;
+		
+		TwoSum sum = new TwoSum();
+		int[] result = sum.solution(array, target);
+		
+//		int[] expected = {};
+//		Assert.assertArrayEquals(expected, sum.twoSum(array, target));
+		for(int i = 0; i < result.length; i++)
+		{
+			System.out.print("index=" + result[i]);
+		}
+	}
+}
